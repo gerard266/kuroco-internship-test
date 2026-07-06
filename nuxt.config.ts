bc96f778a2cd9ecef6b40ae5ec0ae496904cf016
gerard266 <gerard@diverta.co.jp>
2026-07-06 17:05:51 +0900

    Added basic paging.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  ssr: false,
+  runtimeConfig: {
+    // Public keys that are exposed to the client
+    public: {
+      apiBase: "https://gerard-internship-site.g.kuroco.app/",
+    },
+  },
+});
